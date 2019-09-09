559c9c3dff3df7d6fd8e8472b430168686e5924a
Julián David Muñoz Arias - Ceiba Software <julian.munoz@ceiba.com.co>
2019-09-09 13:39:55 -0500

    nueva conf sonar

--- src/main/java/co/com/ceiba/Reservas/ReservasApplication.java
+++ src/main/java/co/com/ceiba/Reservas/ReservasApplication.java
@@ -3,8 +3,12 @@ package co.com.ceiba.Reservas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import co.com.ceiba.reserva.dominio.Bill;
+
 @SpringBootApplication
 public class ReservasApplication {
+	
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservasApplication.class, args);
--- src/main/java/co/com/ceiba/reserva/dominio/Bill.java
+++ src/main/java/co/com/ceiba/reserva/dominio/Bill.java
@@ -1,5 +1,7 @@
 package co.com.ceiba.reserva.dominio;
 
+import java.util.Calendar;
+
 import co.com.ceiba.reserva.dominio.Reservation;
 
 public class Bill {
@@ -7,6 +9,7 @@ public class Bill {
 	private float price;
 	private int discountForPeople;
 	private int discpuntForDays;
+	private Reservation reservation;
 
 	public Bill(float price, int discountForPeople, int discpuntForDays, Reservation reservation) {
 		this.price = price;
@@ -38,7 +41,19 @@ public class Bill {
 	public void setDiscpuntForDays(int discpuntForDays) {
 		this.discpuntForDays = discpuntForDays;
 	}
+	
+	public float discountForFivePeopleOrMore() {
+		if(reservation.getNumberPeople()>=5) {
+			discountForPeople*=price/100;
+			price-=discountForPeople;
+		}
+		return price;
+	}
+	
+	
+	
+	
+	}
 
 
 
-}
