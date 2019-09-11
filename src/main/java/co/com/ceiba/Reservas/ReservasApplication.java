0d84aa9282f6d97eb8e91fac951f90b1d73a5a87
Julián David Muñoz Arias - Ceiba Software <julian.munoz@ceiba.com.co>
2019-09-11 14:28:31 -0500

    tengo miedo

--- src/main/java/co/com/ceiba/Reservas/ReservasApplication.java
+++ src/main/java/co/com/ceiba/Reservas/ReservasApplication.java
@@ -3,7 +3,7 @@ package co.com.ceiba.Reservas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import co.com.ceiba.reserva.dominio.Bill;
+
 
 @SpringBootApplication
 public class ReservasApplication {
